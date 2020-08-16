4b09993d57aa31e9953c36a52cf2d9bbd89d21af
mustii <mustii@mmk.one>
2020-08-16 15:47:20 +0300

    Initial Commit

---
+++ Favoriteplace/FavoriteplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteplaceApp.swift
+//  Favoriteplace
+//
+//  Created by Mustafa Khalil on 8/16/20.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
